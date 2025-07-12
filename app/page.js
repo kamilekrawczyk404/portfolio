daf2cfd5588c32ff909e2e6515c36d51da2c1897
kamilekkrawczyk404 <kamilekkrawczyk404@gmail.com>
2025-07-12 16:44:35 +0200

    feat: vercel url?

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const fetcher = (url, params = {}) =>
   fetch(
     `${
       process.env.VERCEL_URL
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        ? `https://${process.env.VERCEL_URL}`
         : "http://localhost:3000"
     }${url}`,
     params,
